c60cb568bcc851159d16f670dec63df796d4f5c4
Manuel Kehl <mank319@zoho.com>
2015-05-03 15:23:40 +0200

    Added the runtime Gtk distribution
    This is to be shipped with the Windows executable. It contains
    customizations like particular icons and gtk settings files for
    better font rendering and a better overall "look and feel" on Windows.

---
+++ dist/share/gdb/auto-load/libgobject-2.0.so.0.3400.3-gdb.py
@@ -0,0 +1,10 @@
+import sys
+import gdb
+
+# Update module path.
+dir_ = '/srv/win32builder/fixed_364/build/win32/share/glib-2.0/gdb'
+if not dir_ in sys.path:
+    sys.path.insert(0, dir_)
+
+from gobject import register
+register (gdb.current_objfile ())
