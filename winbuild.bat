6a2759770e2d424b41855a58cae292a8f5670e2f
Manuel Kehl <mank319@zoho.com>
2015-05-15 00:01:37 +0200

    Updated the buildscript
    Now it covers icon generation via windres

--- winbuild.bat
+++ winbuild.bat
@@ -1,1 +1,2 @@
-valac --pkg gtk+-3.0 -X -mwindows src\*.vala src\view\*.vala
+windres data\app-icon.rs -o data\app-icon.o
+valac --pkg gtk+-3.0 -X data\app-icon.o -X -mwindows src\*.vala src\view\*.vala
